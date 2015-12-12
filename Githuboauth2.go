e03aa10e0d46036d51e03d170653ca9be51d1305
andreas_westberg <andreaswestberg91@gmail.com>
2015-12-12 13:49:25 +0100

    Callback wip

--- Githuboauth2.go
+++ Githuboauth2.go
@@ -6,8 +6,13 @@ import (
     "github.com/google/go-github/github"
     "golang.org/x/oauth2"
     githuboauth "golang.org/x/oauth2/github"
+    "net/url"
 )
 
+const(
+  defaultBaseURL = "https://api.github.com/"
+  mediaTypeV3      = "application/vnd.github.v3+json"
+)
 //auth/github
 //auth/github/callback
 // You must register the app at https://github.com/settings/applications
@@ -64,4 +69,86 @@ func (c *OauthConf) HandleGitHubCallback(w http.ResponseWriter, r *http.Request)
     }
     fmt.Printf("Logged in as GitHub user: %s\n", *user.Login)
     http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+}
+
+func (s *UsersService) GetUser() (*User, *Response, error) {
+	var u string
+	u = "user"
+
+	req, err := GetUserRequest()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	uResp := new(User)
+	resp, err := DoGetUser(req, uResp)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return uResp, resp, err
+}
+
+func GetUserRequest() (*http.Request, error) {
+	rel, err := url.Parse("user")
+	if err != nil {
+		return nil, err
+	}
+  baseURL, _ := url.Parse(defaultBaseURL)
+	u := baseURL.ResolveReference(rel)
+
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Accept", mediaTypeV3)
+
+	return req, nil
+}
+
+func DoGetUser(req *http.Request, v interface{}) (*Response, error) {
+  // HTTP client used to communicate with the API.
+	client := http.DefaultClient
+  resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	response := newResponse(resp)
+
+	err = CheckResponse(resp)
+	if err != nil {
+		// even though there was an error, we still return the response
+		// in case the caller wants to inspect it further
+		return response, err
+	}
+
+	if v != nil {
+		if w, ok := v.(io.Writer); ok {
+			io.Copy(w, resp.Body)
+		} else {
+			err = json.NewDecoder(resp.Body).Decode(v)
+		}
+	}
+	return response, err
+}
+
+// CheckResponse checks the API response for errors, and returns them if
+// present.  A response is considered an error if it has a status code outside
+// the 200 range.  API error responses are expected to have either no response
+// body, or a JSON response body that maps to ErrorResponse.  Any other
+// response body will be silently ignored.
+func CheckResponse(r *http.Response) error {
+	if c := r.StatusCode; 200 <= c && c <= 299 {
+		return nil
+	}
+	errorResponse := &ErrorResponse{Response: r}
+	data, err := ioutil.ReadAll(r.Body)
+	if err == nil && data != nil {
+		json.Unmarshal(data, errorResponse)
+	}
+	return errorResponse
 }
